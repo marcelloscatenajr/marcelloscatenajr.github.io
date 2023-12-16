d074cda033b32d90e404c6bf73e536cbc06de881
Marcello Scatena Junior <36850942+marcelloscatenajr@users.noreply.github.com>
2023-12-15 21:54:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function precoKaraoke() {
-    var preco = Number(324/document.getElementById("quantidade").value).toFixed(2);
+    var preco = Number(600/document.getElementById("quantidade").value).toFixed(2);
     document.getElementById("preco").innerHTML = `R$${preco} para cada um!`
 }
